29ddc1454160ce4dcc3d749468af76179b4d2a74
sc17ljth <sc17ljth@leeds.ac.uk>
2019-03-07 19:58:06 +0000

    fixed small let statement bug

--- src/com/Parser.java
+++ src/com/Parser.java
@@ -212,7 +212,6 @@ public class Parser {
             this.t.getNextToken();
             parseExpression();
             parseSymbol("]");
-            this.t.getNextToken();
         }
         parseSymbol("=");
         parseExpression();
